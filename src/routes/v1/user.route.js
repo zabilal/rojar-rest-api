2f8e1cc04b300e38252113bc75b86876f73327b7
zakariyya <zakariyyaraji@gmail.com>
2021-06-08 14:30:46 +0100

    removing user validations by Joi

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -8,18 +8,14 @@ const router = express.Router();
 
 router
   .route('/')
-  // .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
-  .post(auth('manageUsers'), userController.createUser)
-  // .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
-  .get(auth('getUsers'), userController.getUsers);
+  .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
+  .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
+  .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
 
 router
   .route('/:userId')
-  // .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
   .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
-  // .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
   .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
-  // .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
   .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
 
 module.exports = router;
