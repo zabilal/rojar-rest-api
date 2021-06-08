cfdb0d819b7e00b6084b7a36aafa334f58d8bdd2
zakariyya <zakariyyaraji@gmail.com>
2021-06-08 20:40:59 +0100

    removing user validation on update

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -15,7 +15,8 @@ router
 router
   .route('/:userId')
   .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
-  .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
+  // .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
+  .patch(auth('manageUsers'), userController.updateUser)
   .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
 
 module.exports = router;
